68650268506c3b9d0072480fe3f6229499d3fe3c
stephane <samodev1725@gmail.com>
2017-09-16 08:20:23 +0200

    Add foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+
 let hashHistory = Router.hashHistory;
 
 var routes = (
